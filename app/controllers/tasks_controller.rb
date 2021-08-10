a41d9fbd17dfedd316fae99eb12d5d231d456cc2
Louis Labes <labeslouis@gmail.com>
2021-08-10 13:42:55 +0200

    Vers. 2

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,35 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+   @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(params_task)
+    @task.save
+    redirect_to tasks_path(@tasks)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to destroy_task_path(@tasks)
+  end
+
+  private
+
+  def params_task
+    params.require(:task).permit(:title)
+  end
+end
